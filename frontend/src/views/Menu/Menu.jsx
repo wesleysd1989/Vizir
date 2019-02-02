6fd77f5e636ac0efd1431a668330f116e9c9fba9
wesley dos santos dias <wesleysd1989@gmail.com>
2019-02-02 11:33:31 -0200

    add logo on menu

--- frontend/src/views/Menu/Menu.jsx
+++ frontend/src/views/Menu/Menu.jsx
@@ -1,13 +1,17 @@
 
 import React, { Component } from 'react'
 
+import logo from '../../assets/logo.png'
+
 class Menu extends Component {
 
     render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container">
-                    <a className="navbar-brand mb-0 h1" href="/#">telzir</a>
+                    <a className="navbar-brand mb-0 h1" href="/#">
+                    <img src={logo} width="30" height="30" className="d-inline-block align-top" alt="logo"/>
+                    telzir</a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
