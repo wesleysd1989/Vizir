329fb5376d8246da9b1a0523aff1e2549bee57fe
wesley dos santos dias <wesleysd1989@gmail.com>
2019-02-02 18:19:17 -0200

    Update Planos.jsx

--- frontend/src/views/Planos/Planos.jsx
+++ frontend/src/views/Planos/Planos.jsx
@@ -10,32 +10,32 @@ class Planos extends Component {
                     <h1 className="text-center" style={{ color: 'white' }}>PLANOS</h1>
                     <div className="row">
                     <div className="col-sm-4">
-                            <div className="card my-3">
+                            <div className="card my-3 rounded-0">
                                 <div className="card-body">
                                     <h4 className="card-title"><b>FaleMais 30</b></h4>
                                     <h5 className="card-text" style={{color: 'blue'}}>30 minutos</h5>
                                     <hr/>
-                                    <button className="btn btn-primary my-4 my-sm-0" type="submit">Mais informações</button>
+                                    <button className="btn btn-primary my-4 my-sm-0" disabled type="submit">Mais informações</button>
                                 </div>
                             </div>
                     </div>
                     <div className="col-sm-4">
-                            <div className="card my-3">
+                            <div className="card my-3 rounded-0">
                                 <div className="card-body">
                                     <h4 className="card-title"><b>FaleMais 60</b></h4>
-                                    <h5 className="card-text" style={{color: 'blue'}}>30 minutos</h5>
+                                    <h5 className="card-text" style={{color: 'blue'}}>60 minutos</h5>
                                     <hr/>
-                                    <button className="btn btn-primary my-4 my-sm-0" type="submit">Mais informações</button>
+                                    <button className="btn btn-primary my-4 my-sm-0" disabled type="submit">Mais informações</button>
                                 </div>
                             </div>
                     </div>
                     <div className="col-sm-4">
-                            <div className="card my-3">
+                            <div className="card my-3 rounded-0">
                                 <div className="card-body">
                                     <h4 className="card-title"><b>FaleMais 120</b></h4>
-                                    <h5 className="card-text" style={{color: 'blue'}}>30 minutos</h5>
+                                    <h5 className="card-text" style={{color: 'blue'}}>120 minutos</h5>
                                     <hr/>
-                                    <button className="btn btn-primary my-4 my-sm-0" type="submit">Mais informações</button>
+                                    <button className="btn btn-primary my-4 my-sm-0" disabled type="submit">Mais informações</button>
                                 </div>
                             </div>
                     </div>
